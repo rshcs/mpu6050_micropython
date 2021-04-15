8c08d9bb3eebf5a063c4419732183f8e771720b1
RoshanCS <roshan.cs790@gmail.com>
2021-04-15 17:02:09 +0530

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,12 +1,25 @@
+import math
 import mpu
 from time import sleep as delay
 
-angle = mpu.Mpu()
+raw_value = mpu.Mpu()
 
+def constrain(inv, minv, maxv):
+    if inv < minv:
+        return minv
+    elif inv > maxv:
+        return maxv
+    else:
+        return inv
 
 while True:
-    print(angle.read_data())
-    delay(2)
+    xaxis = raw_value.read_data()[0]
+    yaxis = raw_value.read_data()[1]
+    zaxis = raw_value.read_data()[2]
+    #print(yaxis, xaxis, zaxis)
+    rad_angle = math.atan2(xaxis , yaxis)
+    print(math.degrees(rad_angle)+2.5)
+    delay(.1)
 
 
 
