83bfb03afeb7b244822a817591a05e4955fbb477
RoshanCS <roshan.cs790@gmail.com>
2021-04-16 12:09:20 +0530

    Constrain function removed

--- tester.py
+++ tester.py
@@ -4,14 +4,6 @@ from time import sleep as delay
 
 raw_value = mpu.Mpu()
 
-def constrain(inv, minv, maxv):
-    if inv < minv:
-        return minv
-    elif inv > maxv:
-        return maxv
-    else:
-        return inv
-
 while True:
     xaxis = raw_value.read_data()[0]
     yaxis = raw_value.read_data()[1]
